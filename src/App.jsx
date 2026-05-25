35556d648d482011297a299dd9f4e102c9292dd6
cctve <cctvedepan@gmailcom>
2026-05-25 23:47:49 +0700

    upload dashboard baru

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#f5f5f5",
+      padding: "20px",
+      fontFamily: "Arial"
+    }}>
+      <h1 style={{ color: "green" }}>
+        Dashboard Internet
+      </h1>
+
+      <div style={{
+        background: "white",
+        padding: "20px",
+        borderRadius: "10px",
+        marginTop: "20px"
+      }}>
+        <h2>Status Jaringan</h2>
+        <p>Internet Normal</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
